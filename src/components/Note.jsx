9543376913ac973653c38a85e77eeef9d44623ca
cacashitoshito <95678739+cacashitoshito@users.noreply.github.com>
2023-11-28 12:04:34 +0100

    Add files via upload

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note(){
+    return <div className="note">
+       <h1>The Lost Book</h1>
+       <p>this is the lost book<br/>Of samundaa</p> 
+    </div>
+}
+
+export default Note;
